90935a84874849428fb9e091bdecb5f2715a6495
Tomasz Mrzy <lion137@gmail.com>
2019-01-25 18:32:38 +0000

    generics started, mean

--- linear_time_selec.cpp
+++ linear_time_selec.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <vector>
 #include <random>
 void Swap(int a[], int l, int r);
-int partition(int a[], int n, int p);
+template <typename T, typename I>
+T partition(std::vector<T>, I s, I p);
 int partition2(int a[], int s, int p);
 int partition_random(int a[], int s, int p);
 int quick_select(int a[], int s, int n, int k);
 int quick_select2(int a[], int s, int n, int k);
+float median(int a [], int n);
 void printAr(int a[], int n);
 void quicksort(int a[], int s, int n) {
 	if (s < n) {
@@ -16,16 +19,17 @@ void quicksort(int a[], int s, int n) {
 } 
 // ----------------MAIN-----------------
 int main() {
-	int A[10] = {0,5, 1, 10, -9, 801, -388, 137, 123, -9};
-	printAr(A, 10);
-	int s = 9;
-	for (int i = 1; i < 11;i++){
-		int e = quick_select2(A, 0, 9, i);
+	int A[8] = {1, 10, -9, 801, -388, 137, 123, -9};
+	printAr(A, 8);
+	int s = 8;
+	for (int i = 1; i < 9;i++){
+		int e = quick_select2(A, 0, 8, i);
 		std::cout << i <<"th largest:"<< e << " \n";
 	}
+	std::cout << median(A, 8) << " <- median";
 	std::cout << "\n";
-	quicksort(A, 0, 9);
-	printAr(A, 10);
+	quicksort(A, 0, 8);
+	printAr(A, 8);
 	std::cout << "\n";
 
 	return 0;
@@ -43,18 +47,6 @@ void Swap(int a[], int l, int r){
 	
 }
 
-int partition(int a[], int n, int p) {
-	Swap(a, p, n);
-	int l = 0;
-	for (int i = 1; i <= n - 1; i++) {
-		if (a[i] <= a[n]) {
-			l += 1;
-			Swap(a, l, i);
-		}
-	}
-	Swap(a, p, l + 1);
-	return l + 1;
-}
 
 int quick_select(int a[], int s, int n, int k) {
 	if (n == 1) {return a[1];}
@@ -78,10 +70,12 @@ int partition2(int a[], int p, int r) {
 	for (int j = p; j < r;j++){
 		if (a[j] <= t) {
 			i += 1;
-			Swap(a, i, j);
+			//Swap(a, i, j);
+			std::swap(a[i], a[j]);
 		}
 	}
-	Swap(a, i + 1, r);
+	//Swap(a, i + 1, r);
+	std::swap(a[i + 1], a[r]);
 	return i + 1;
 }
 
@@ -105,9 +99,40 @@ int partition_random(int a[], int p, int r) {
 	std::mt19937 rng(rd());   
 	std::uniform_int_distribution<int> uni(p, r); 
 	auto rn = uni(rng);
-	Swap(a, r, rn);	
+	std::swap(a[r], a[rn]);
 	return partition2(a, p, r);
 }
+
+float median(int a [], int n) {
+	if (n % 2 == 1)
+		return quick_select2(a, 0, n - 1, n / 2 + 1);
+	int l = n / 2;
+	int r = n / 2 + 1;
+	return (1.0 * (quick_select2(a, 0, n - 1, l) + 
+			quick_select2(a, 0, n - 1, r))) / 2;
+	
+}
+
+// generics
+template <typename T, typename I>
+I partition(std::vector<T> a, I p, I r){
+	T t = a[r];
+	I i = p - 1;
+	for (I j = p; j < r;j++){
+		if (a[j] <= t) {
+			i += 1;
+			std::swap(a[i], a[j]);
+		}
+	}
+	std::swap(a[i + 1], a[r]);
+	return i + 1;	
+}
+
+
+
+
+
+
 
 
 
